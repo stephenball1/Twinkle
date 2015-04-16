33e70e284585fbaa10488e9fd114e74114268f6d
Judy Mou <moujudy@gmail.com>
2015-04-15 23:59:38 -0700

    new firebase ref

---
+++ app/src/main/java/com/launch/twinkle/twinkle/data/FirebaseRef.java
@@ -0,0 +1,7 @@
+package com.launch.twinkle.twinkle.data;
+
+/**
+ * Created by judymou on 4/15/15.
+ */
+public class FirebaseRef {
+}
---
+++ app/src/main/java/com/launch/twinkle/twinkle/models/FirebaseRef.java
@@ -0,0 +1,7 @@
+package com.launch.twinkle.twinkle.models;
+
+/**
+ * Created by judymou on 4/15/15.
+ */
+public class FirebaseRef {
+}
